b84b0d555d8d3a98461a998440880d1d2a82d212
Krishnan Shankar <krishnans2006@gmail.com>
2022-01-11 02:30:46 +0000

    Fix token
    Don't worry - the old token does not work anymore. Sorry!

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from threading import Thread
+from dotenv import load_dotenv
 
 from bot import client, update_status
 from app import run_webserver
+
+load_dotenv()
 
 webserver = Thread(target=run_webserver)
 webserver.start()
 
 update_status.start()
 
-client.run("ODc3MDE3MTA1NjIxNDc1MzM4.YRsf1A.aqAUNryGWW9vdG6horsAIbAh2qM")
+client.run(os.getenv("TOKEN"))
